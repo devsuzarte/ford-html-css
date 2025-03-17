f54935a8a8257dc029d8235fec29af0540e41fe0
devsuzarte <ben.dantassuzarte@gmail.com>
2025-03-17 09:34:51 -0300

    add animation for the carousel

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -3,27 +3,42 @@ const imgTag = document.getElementById("imagem-principal");
 
 // Carousel
 class Carousel {
-    constructor(element, images) {
+    constructor(element, infos) {
         this._element = element;
-        this._images = images;
+        this._infos = infos;
     }
 
-    async start(interval) {
+    start(interval) {
         let curr = 0
 
-        await setInterval(() => {
-            this._element.setAttribute("src", this._images[curr])
-            curr === this._images.length - 1 ? curr = 0 : curr++
+        setInterval(() => {
+
+            this._element.classList.add('fade-out')
+
+            setTimeout(() => {
+                this._element.setAttribute("src", this._infos[curr].img)
+
+                this._element.classList.remove('fade-out')
+                this._element.classList.add('fade-in')
+
+            }, 1000)
+
+            curr === this._infos.length - 1 ? curr = 0 : curr++
         }, interval);
     }
 }
 
-const images = [
-    "../assets/img/imagem_1.jpg",
-    "../assets/img/imagem_2.jpg",
-    "../assets/img/imagem_3.jpg"
+// Variables
+const infos = [
+    { title: "FORD RANGER 2022", img: "../assets/img/imagem_1.jpg", url: "lancamento.html" },
+    { title: "FORD RANGER 2022", img: "../assets/img/imagem_2.jpg", url: "lancamento.html" },
+    { title: "FORD RANGER 2022", img: "../assets/img/imagem_3.jpg", url: "lancamento.html" }
 ]
 
-const c = new Carousel(imgTag, images);
+// "../assets/img/imagem_1.jpg",
+// "../assets/img/imagem_2.jpg",
+// "../assets/img/imagem_3.jpg"
 
-c.start(5000);
+const carousel = new Carousel(imgTag, infos);
+
+carousel.start(10000);
